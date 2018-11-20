13716269595affdd8fcad118cd9a417c7d4af5f4
Jonathan Bradshaw <j0nathanb.dev@gmail.com>
2018-11-20 11:02:21 -0800

    Sockets online, but not broadcasting to room

--- src/components/remote.js
+++ src/components/remote.js
@@ -1,13 +1,29 @@
 /* jshint esversion: 6 */
 import React, { Component } from 'react';
+import io from 'socket.io-client';
 
 class Remote extends Component {
   constructor(props) {
     super(props)
     
-    this.playPause = this.playPause.bind(this)
+    this.state = {
+      timestamp: null,
+    }
+
+    this.playPause = this.playPause.bind(this);
   }
-  
+
+  componentDidMount() {
+    const socket = io('http://localhost:3001');
+
+    const cb = ((err, timestamp) => this.setState({ 
+      timestamp 
+    }, console.log(`time: ${timestamp}`)));
+
+    socket.on('timer', timestamp => cb(null, timestamp));
+    socket.emit('subscribeToTimer', 1000);
+  }
+
   playPause() {
     this.props.handleClick(' ');
     console.log('play/pause');
---
+++ src/server.js
@@ -0,0 +1,46 @@
+var app = require('express')();
+var http = require('http').Server(app);
+const io = require('socket.io')(http, { origins: '*:*'});
+const port = process.env.port || 3001;
+
+app.get('/', function(req, res){
+  res.sendFile('../public/index.html', {root: __dirname});
+});
+
+io.on('connection', function (client) {
+  console.log('connected to socket');
+
+  client.on('subscribeToTimer', (interval) => {
+    console.log('client is subscribing to timer with interval ', interval);
+    setInterval(() => {
+      client.emit('timer', new Date());
+    }, interval);
+  });
+
+  // client.on('next', handleRegister)
+
+  // client.on('previous', handleJoin)
+
+  // client.on('play', handleLeave)
+
+  // client.on('pause', handleMessage)
+
+  // client.on('ffwd', handleGetChatrooms)
+
+  // client.on('rwd', handleGetAvailableUsers)
+
+  client.on('disconnect', function () {
+    console.log('client disconnect...', client.id)
+   //handleDisconnect()
+  })
+
+  client.on('error', function (err) {
+    console.log('received error from client:', client.id)
+    console.log(err)
+  })
+});
+
+http.listen(port, function (err) {
+  if (err) throw err;
+  console.log(`listening on ${port}`);
+});
