c6da01f846509f0ccbda90a2cb225f47412b91c0
Jonathan Bradshaw <j0nathanb.dev@gmail.com>
2018-11-25 22:40:48 -0800

    Create environment variables

--- src/clients/sockets.js
+++ src/clients/sockets.js
@@ -1,9 +1,9 @@
 /* jshint esversion: 6 */
 import io from 'socket.io-client';
 
-const server = process.env.NODE_ENV === 'development' ? 
-  'http://localhost:3001' : 
-  '/';
+const server = process.env.NODE_ENV === 'development' 
+  ? process.env.REACT_APP_LOCALHOST
+  : process.env.REACT_APP_EC2
 
 const Socket = io(server);
 export default Socket;
