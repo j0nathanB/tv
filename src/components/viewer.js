e9a67b0456db51490c96d74794621c5a9915cf82
Jonathan Bradshaw <j0nathanb.dev@gmail.com>
2018-12-15 23:25:00 -0800

    Add full screen static

--- src/components/viewer.js
+++ src/components/viewer.js
@@ -1,31 +1,49 @@
 import React, { Component } from 'react';
-import television from '../assets/tv.png'
+import television from '../assets/tv_small.png'
 import tvStatic from '../assets/tv_static.gif'
 
 class Viewer extends Component {
-  
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      isLoading: true
+    }
+
+    this.showStatic = this.showStatic.bind(this)
+    this.timer = setTimeout(this.showStatic, 250)
+  }
+
+  showStatic() {
+    this.setState({ isLoading: false });
+  }
+
+  componentWillUnmount(){
+    clearTimeout(this.timer);
+  }
+
   render() {
-    const { isConnected } = this.props;
+    const { isConnected, isLoading } = this.props;
     const codeStyle = isConnected ? "room-code room-code-active" : "room-code" 
     const tvImage = isConnected 
       ? null
-      : <img className="static" alt="static" src={tvStatic} /> 
+      : <div className="iframe" id="player"/>
 
 
     return (
       <div>
-        {/* <h1 className={codeStyle}>{this.props.roomId}</h1> */}
+        {this.state.isLoading ? <img className="tvStatic" src={tvStatic} /> : null}
         <div className="viewer">
+          <img className="television" alt="television" src={television} />
+
           {isConnected ? null : (<div className="instructions">
             <p>Instructions: </p>
             <p>1. Open highlandtv.net on a mobile device. </p>
             <p>2. Enter this code: <span className={codeStyle}>{this.props.roomId}</span></p>
             <p>3. Enjoy the show!</p>
           </div>)}
-          
-          <img className="television" alt="television" src={television} />
 
-          <div className="iframe" id="player"/>
+          {tvImage}
         </div>
       </div>
     ) 
